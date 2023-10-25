3d619ab21420ea52dbd6986c9da9ad448db75af2
Alexander8421 <Alexander.Asmus@smail.inf.h-brs.de>
2023-10-25 09:19:52 +0200

    create Container and ConcreteMember classes
    create Container and ConcreteMember classes and implement methods

---
+++ src/org/hbrs/se1/ws23/uebung2/Container.java
@@ -0,0 +1,32 @@
+package org.hbrs.se1.ws23.uebung2;
+import javax.swing.*;
+import java.util;
+import java.util.ArrayList;
+
+public class Container {
+
+
+    java.util.ArrayList theContainer = new java.util.ArrayList<Member>();
+
+    public void addMember(Member member) throws ContainerException{
+        theContainer.add(member);
+
+    }
+    public String deleteMember(Integer id){
+        //If
+        for(i=0; i<theContainer.size();i++){
+            if (theContainer[i].getID() = id){break;}
+        }
+
+        theContainer.remove(theContainer[i]);
+        return "Member mit ID "+id+" nicht gefunden";
+    }
+    public void dumb(){
+        for (i=0; i<theContainer.size(); i++){}
+        System.out.println(theContainer[i].toArray());
+    }
+
+    public int size(){
+        return theContainer.size();
+    }
+}

5b36f27dddf535fd91a202191025958fde0d53e0
Alexander8421 <Alexander.Asmus@smail.inf.h-brs.de>
2023-10-25 11:12:39 +0200

    Update Container.java
    Fehler korrigiert

--- src/org/hbrs/se1/ws23/uebung2/Container.java
+++ src/org/hbrs/se1/ws23/uebung2/Container.java
@@ -1,7 +1,4 @@
 package org.hbrs.se1.ws23.uebung2;
-import javax.swing.*;
-import java.util;
-import java.util.ArrayList;
 
 public class Container {
 
@@ -9,24 +6,27 @@ public class Container {
     java.util.ArrayList theContainer = new java.util.ArrayList<Member>();
 
     public void addMember(Member member) throws ContainerException{
-        for(i=0; i<theContainer.size();i++){
-            if (theContainer[i].getID() = member.getID()){throw new ContainerException(("Das Member-Objekt mit der ID "+member.getID()+" ist bereits vorhanden!"));}
+        for(int i=0; i<theContainer.size();i++){
+            Member current=(Member) theContainer.get(i);
+            if (current.getID() == member.getID()){throw new ContainerException(("Das Member-Objekt mit der ID "+member.getID()+" ist bereits vorhanden!"));}
         }
         theContainer.add(member);
 
     }
     public String deleteMember(Integer id){
         //If
+        int i;
         for(i=0; i<theContainer.size();i++){
-            if (theContainer[i].getID() = id){break;}
+            Member current = (Member)theContainer.get(i);
+            if (current.getID() == id){break;}
         }
 
-        theContainer.remove(theContainer[i]);
+        theContainer.remove(theContainer.get(i));
         return "Member mit ID "+id+" nicht gefunden";
     }
     public void dumb(){
-        for (i=0; i<theContainer.size(); i++){}
-        System.out.println(theContainer[i].toArray());
+        for (int i=0; i<theContainer.size(); i++){System.out.println(theContainer.get(i).toString());}
+
     }
 
     public int size(){
